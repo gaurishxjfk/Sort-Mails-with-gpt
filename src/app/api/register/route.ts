9434daf376877dfe5193fc76ddea67b18fa4f9eb
thinkathonacl <aclthinkathon@gmail.com>
2025-03-14 21:41:24 +0530

    test

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    const body = await req.json()
+    console.log(body)
+    return NextResponse.json({
+        message: "Done!"
+    })
+}
+
+export async function GET(){
+
+    return NextResponse.json({
+        message: "Done!"
+    })
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,35 @@ export default function Home() {
   const { data: session } = useSession();
   if (session) redirect(`/gmail`);
 
+  async function handleSubmit(){
+    try {
+      const resp = await fetch('/api/register',{
+        method: "POST",
+        // headers: {
+        //   'Content-Type': 'application/json'
+        // },
+        body: JSON.stringify({email: "a@b.com"})
+      })
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
   return (
     <main className="text-center flex flex-col w-[95%] md:w-[85%] lg:w-[65%]  mx-auto h-screen ">
       <div className=" bg-white rounded-md my-auto ">
-          <p className="text-gray-500 text-[12px] mt-2">Not signed in</p>
+          {/* <p className="text-gray-500 text-[12px] mt-2">Not signed in</p>
           <Button
             text={"Login with Google"}
             btnHandler={signIn}
             className="bg-blue-600 my-2"
-          />
+          /> */}
+          <input type="text" placeholder="enter email" />
+          <Button
+            text={"Login with Google"}
+            btnHandler={handleSubmit}
+            className="bg-blue-600 my-2"
+          /> 
       </div>
     </main>
   );
